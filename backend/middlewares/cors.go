74b9fddd4f2f901416a56f6a1f77ab7fe7367616
KaiserProger <kaisergrobe@gmail.com>
2023-05-28 17:34:52 +0700

    Return hard-code IPs

--- backend/middlewares/cors.go
+++ backend/middlewares/cors.go
@@ -9,7 +9,7 @@ import (
 func Cors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		start := time.Now()
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000") // SET FRONEND ADDRESS WHEN COMES TO PRODUCTION!!!
+		w.Header().Set("Access-Control-Allow-Origin", "http://80.243.141.49:3000") // SET FRONTEND ADDRESS WHEN COMES TO PRODUCTION!!!
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		next.ServeHTTP(w, req)
 		log.Printf("%s %s %s", req.Method, req.RequestURI, time.Since(start))
