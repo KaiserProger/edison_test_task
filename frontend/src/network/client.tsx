b27e640d00d3e20a2521ecdc6bfff42bcfc9c65d
KaiserProger <kaisergrobe@gmail.com>
2023-05-28 17:28:14 +0700

    Revert to localhost

--- frontend/src/network/client.tsx
+++ frontend/src/network/client.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const baseApiClient = axios.create({
-    baseURL: 'http://backend:8080/',
+    baseURL: 'http://localhost:8080/',
     timeout: 1000,
     withCredentials: true,
 });
